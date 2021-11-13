e2879f3cc95616bb8755ce45984bf6ff10aefa1b
Reza Ghasemi <rezaghasemi.cs@gmail.com>
2021-11-14 02:28:07 +0330

    Voters and Candidates storages are made

--- contracts/WE.sol
+++ contracts/WE.sol
@@ -1,5 +1,28 @@
 pragma solidity ^0.5.0;
 
 contract WE {
-   constructor() public {}
+
+   struct Candidate{
+      uint id;
+      string nick_name;
+      uint votes;
+   }
+
+   struct Voter{
+      uint id;
+      string nick_name;
+      uint eth;
+   }
+
+
+   mapping(uint => Voter) public Voters;
+   mapping(uint => Candidate) public Candidates;
+
+
+
+
+   constructor(uint numOfparticipants) public {
+     
+
+   }
 }
