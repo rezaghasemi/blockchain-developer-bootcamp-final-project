aa51e65230a733dda2ef60e984862ac372967a5b
Reza Ghasemi <rezaghasemi.cs@gmail.com>
2021-11-16 02:30:31 +0330

    Vote function should be completed

--- contracts/WE.sol
+++ contracts/WE.sol
@@ -1,12 +1,29 @@
 pragma solidity 0.5.16;
 
 contract WE {
+
+   // variables    /////////////////////////////////////////////////////////////////////////////////////////////////
+
    address owner;
+
+   uint nOv;
+   uint nOc;
+   uint public nOc_counter;
+   uint public nOv_counter;
+
+   mapping(address => Voter) public Voters;
+   mapping(uint => Candidate) public Candidates;
+   
+
+   // modeifiers   /////////////////////////////////////////////////////////////////////////////////////////////////
+
 
    modifier onlyOwner {
       require(msg.sender == owner);
       _;
    }
+
+   // structures   /////////////////////////////////////////////////////////////////////////////////////////////////
 
    struct Candidate{
       uint id;
@@ -17,35 +34,43 @@ contract WE {
    struct Voter{
       uint id;
       string nick_name;
+      address add;
       uint eth;
    }
+
+   // States      /////////////////////////////////////////////////////////////////////////////////////////////////
+
    enum state{REG_CANDIDATES,REG_VOTERS, VOTING, FINISH}
+   state contract_state = state.REG_CANDIDATES;
 
-
-   mapping(uint => Voter) public Voters;
-   mapping(uint => Candidate) public Candidates;
-   uint nOv;
-   uint public nOc;
+   
+   
+   // functions  /////////////////////////////////////////////////////////////////////////////////////////////////
 
    function reg_candidate(string memory _name) public onlyOwner {
       //candidates should be added
+      if(nOc_counter==nOc){contract_state=state.VOTING;}
+      require(nOc_counter<=nOc || contract_state==state.REG_CANDIDATES, "Sorry there is an error!");
       nOc_counter++;
       Candidates[nOc_counter]=Candidate(nOc_counter,_name, 0);
    }
 
-   function reg_voter() public payable {
-      //register an voter and gets 1 eth for guarantee
 
-
-   }
-
-   function vote(uint candidate_id) public {
+   function vote(uint candidate_id) payable public {
       //add votes for the selected candidate
+      require(contract_state==state.VOTING || msg.value>=10^18,"Sorry there is an error!");
+
+
+      // here
+      
+      
+
    }
-   uint public nOc_counter;
-   uint public nOv_counter;
+   
 
-
+   // constractor    ///////////////////////////////////////////////////////////////////////////////////////////
+   
+   
    constructor(uint numOfcandidates, uint numOfvoters) public {
       owner=msg.sender;
       nOv=numOfvoters;
